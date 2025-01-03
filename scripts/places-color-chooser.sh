aa641dd2a15d7294db03918646a861d870e8b404
François Vantomme <francois.vantomme@enercoop.org>
2025-01-03 22:54:15 +0100

    feat(scripts): allow levels of verbosity in places-color-chooser

--- scripts/places-color-chooser.sh
+++ scripts/places-color-chooser.sh
@@ -24,6 +24,9 @@ Options:
   -n, --dry-run               perform a trial run with no changes made
   -v, --verbose               increase verbosity"
 
+DRYRUN=0
+VERBOSE=0
+
 # options
 while [[ "$#" -gt 0 && "$1" =~ ^- && ! "$1" == "--" ]]; do case "$1" in
   -c | --color )
@@ -42,20 +45,17 @@ while [[ "$#" -gt 0 && "$1" =~ ^- && ! "$1" == "--" ]]; do case "$1" in
     DRYRUN=1
     ;;
   -v | --verbose )
-    VERBOSE=1
+    VERBOSE+=1
     ;;
 esac; shift; done
 if [[ "$#" -gt 0 && "$1" == '--' ]]; then shift; fi
-
-DRYRUN="${DRYRUN:-0}"
-VERBOSE="${VERBOSE:-0}"
 
 if [[ -f "${scalable_places_directory}/folder-${PLACES_COLOR}.svg" ]]; then
   pushd "${scalable_places_directory}" 1>/dev/null
   for i in $(realpath "*-${PLACES_COLOR}*.svg"); do
     filename="${i##*/}"
 
-    if [[ ${DRYRUN} -eq 1 || ${VERBOSE} -eq 1 ]]; then
+    if [[ ${VERBOSE} -gt 1 ]]; then
       case "${filename}" in
         "bookmarks-${PLACES_COLOR}.svg")
           echo "${filename} → folder-bookmark.svg"
@@ -65,6 +65,8 @@ if [[ -f "${scalable_places_directory}/folder-${PLACES_COLOR}.svg" ]]; then
           echo "${filename} → ${filename/-${PLACES_COLOR}/}"
           ;;
       esac
+    elif [[ ${VERBOSE} -eq 1 ]]; then
+      printf "."
     fi
 
     if [[ ${DRYRUN} -eq 0 ]]; then
@@ -81,10 +83,16 @@ if [[ -f "${scalable_places_directory}/folder-${PLACES_COLOR}.svg" ]]; then
   done
   popd 1>/dev/null
 
-  if [[ ${DRYRUN} -eq 1 ]]; then
-    echo "Nothing done."
-  else
-    echo "Done."
+  if [[ ${VERBOSE} -eq 1 ]]; then
+    printf "\n"
+  fi
+
+  if [[ ${VERBOSE} -ge 1 ]]; then
+    if [[ ${DRYRUN} -eq 1 ]]; then
+      echo "Nothing done."
+    else
+      echo "Done."
+    fi
   fi
 else
   echo "Invalid color: ${PLACES_COLOR}"
